2d57ab839eb12e1cd939e9a87db04ac87cc5bad1
brunoodavid <bruno_davidthome@hotmail.com>
2026-02-23 15:59:58 -0300

    adicionando aulas de manipulação de dados com coleções e linq

---
+++ manipulacao-de-dados-com-colecao-linq/IntroAColecao/Program.cs
@@ -0,0 +1,13 @@
+﻿var diasDaSemana = new string[] { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+
+var carrinho = new List<Produto>()
+{
+    new Produto(){ Nome = "Leite", Preco = 3.99},
+    new Produto() { Nome = "Manteiga", Preco = 3.99 }
+};
+
+class Produto
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+}
