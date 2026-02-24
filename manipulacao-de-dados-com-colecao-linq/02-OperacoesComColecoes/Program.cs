c39c810403fcad9e2f48b930d271d5cd64c1045c
brunoodavid <bruno_davidthome@hotmail.com>
2026-02-24 16:51:16 -0300

    iniciando codigos da atividade 02

---
+++ manipulacao-de-dados-com-colecao-linq/02-OperacoesComColecoes/Program.cs
@@ -0,0 +1,108 @@
+﻿/*
+Seja um aplicativo de gerenciamento de músicas onde os usuários podem organizar suas faixas
+favoritas em playlists personalizadas. Para cada playlist, é essencial que o usuário tenha
+controle total sobre a sequência de reprodução das músicas, permitindo reordená-las
+livremente a qualquer momento. Além disso, o aplicativo precisa oferecer a funcionalidade de
+reprodução aleatória para uma playlist específica, proporcionando uma experiência de audição
+dinâmica e variada, sem, contudo, alterar a ordem original que o usuário definiu. O desafio
+é criar uma estrutura robusta que suporte a adição e remoção eficiente de músicas, a
+reordenação flexível dentro das playlists e a seleção de faixas tanto em modo sequencial
+quanto aleatório.
+*/
+
+//Funcoes que vamos implementar:
+// [ ] Criar as classes para musicas e playlist
+// [ ] Listar musicas da playlist
+// [ ] Adicionar musica à playlist
+// [ ] Obter uma musica especifica da playlist
+// [ ] Remover musica da playlist
+// [ ] Tocar uma musica aleatoria da playlist
+// [ ] Reordenar musicas segundo alguma logica especifica (ex. duracao)
+// [ ] Uma playlist nao pode ter musicas repetidas
+// [ ] Exibir as 10 musicas mais tocadas em todas as playlists (ranking)
+// [ ] Player de musica com:
+// [ ] - Fila de reproducao (para musicas avulsas e/ou playlists)
+// [ ] - Historico de reproducao
+
+
+
+
+using System.Collections;
+
+
+
+var musica1 = new Musica { Titulo = "Que Pais é Esse?", Artista = "Legião Urbana", Duracao = 350 };
+var musica2 = new Musica { Titulo = "Tempo Perdido", Artista = "Legião Urbana", Duracao = 455 };
+var musica3 = new Musica { Titulo = "Pro Dia Nascer Feliz", Artista = "Barão Vermelho", Duracao = 345 };
+var musica4 = new Musica { Titulo = "Eduardo e Mônica", Artista = "Legião Urbana", Duracao = 530 };
+var musica5 = new Musica { Titulo = "Geração Coca-Cola", Artista = "Legião Urbana", Duracao = 350 };
+
+var rockNacional = new Playlist { Nome = "Rock Nacional" };
+
+rockNacional.Add(musica1);
+rockNacional.Add(musica2);
+rockNacional.Add(musica3);
+rockNacional.Add(musica4);
+rockNacional.Add(musica5);
+
+ExibirPlaylist(rockNacional);
+
+void ExibirPlaylist(Playlist playlist)
+{
+    Console.WriteLine($"Tocando as músicas de {playlist.Nome}");
+    foreach (var musica in playlist)
+    {
+        Console.WriteLine($"\t - {musica.Titulo}");
+    }
+}
+public class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+}
+
+public class Playlist : ICollection<Musica>
+{
+    public string Nome { get; set; }
+
+    public List<Musica> Lista = [];
+    public int Count => Lista.Count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(Musica item)
+    {
+        Lista.Add(item);
+    }
+
+    public void Clear()
+    {
+        Lista.Clear();
+    }
+
+    public bool Contains(Musica item)
+    {
+        return Lista.Contains(item);
+    }
+
+    public void CopyTo(Musica[] array, int arrayIndex)
+    {
+        Lista.CopyTo(array, arrayIndex);
+    }
+
+    public IEnumerator<Musica> GetEnumerator()
+    {
+        return Lista.GetEnumerator();
+    }
+
+    public bool Remove(Musica item)
+    {
+        return Lista.Remove(item);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
